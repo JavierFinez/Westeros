8eda86c7c045ea00067192335e7d288e9a2f1fb1
Javier Finez <javierfinezdios@gmail.com>
2018-09-30 23:09:31 +0200

    Delete EpisodeTest.swift

--- WesterosTests/EpisodeTest.swift
+++
@@ -1,9 +0,0 @@
-//
-//  EpisodeTest.swift
-//  WesterosTests
-//
-//  Created by Javier Finez de Dios on 23/9/18.
-//  Copyright © 2018 Alexandre Freire. All rights reserved.
-//
-
-import Foundation
